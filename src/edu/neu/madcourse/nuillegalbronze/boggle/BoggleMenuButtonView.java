8c47ad6b8d4a5cb33148a623df7ed05fc2f64a19
Ben Johnson <johnson.b@husky.neu.edu>
2011-05-20 15:56:56 -0400

    views

---
+++ src/edu/neu/madcourse/nuillegalbronze/boggle/BoggleMenuButtonView.java
@@ -0,0 +1,13 @@
+package edu.neu.madcourse.nuillegalbronze.boggle;
+
+import android.content.Context;
+import android.view.View;
+
+public class BoggleMenuButtonView extends View {
+
+	public BoggleMenuButtonView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/edu/neu/madcourse/nuillegalbronze/boggle/BoggleStatusView.java
@@ -0,0 +1,13 @@
+package edu.neu.madcourse.nuillegalbronze.boggle;
+
+import android.content.Context;
+import android.view.View;
+
+public class BoggleStatusView extends View {
+
+	public BoggleStatusView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
